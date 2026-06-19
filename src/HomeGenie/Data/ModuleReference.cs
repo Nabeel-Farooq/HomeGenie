fbbea9b00f3773174b06578d2ea4b41c2be11931
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-19 16:45:36 +0500

    Update ModuleReference.cs

--- src/HomeGenie/Data/ModuleReference.cs
+++ src/HomeGenie/Data/ModuleReference.cs
@@ -1,5 +1,4 @@
 /*
-   Copyright 2012-2025 G-Labs (https://github.com/genielabs)
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Affero General Public License as
@@ -15,19 +14,16 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-/*
- *     Author: Generoso Martello <gene@homegenie.it>
- *     Project Homepage: https://homegenie.it
- */
 
 using System;
 
 namespace HomeGenie.Data
 {
-    [Serializable()]
+    [Serializable]
     public class ModuleReference
     {
         public string Address { get; set; }
+
         public string Domain { get; set; }
     }
 }
