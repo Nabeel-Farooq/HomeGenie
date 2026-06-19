74d3a470be0f6aa088204146ef539b737a64cf42
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-19 16:46:14 +0500

    Update ModuleParameter.cs

--- src/HomeGenie/Data/ModuleParameter.cs
+++ src/HomeGenie/Data/ModuleParameter.cs
@@ -1,5 +1,4 @@
 /*
-   Copyright 2012-2025 G-Labs (https://github.com/genielabs)
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU Affero General Public License as
@@ -15,10 +14,6 @@
    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-/*
- *     Author: Generoso Martello <gene@homegenie.it>
- *     Project Homepage: https://homegenie.it
- */
 
 using System;
 using System.Globalization;
@@ -26,79 +21,67 @@ using System.Threading;
 using System.Xml.Serialization;
 
 using LiteDB;
-
 using Newtonsoft.Json;
 
 namespace HomeGenie.Data
 {
-
     /// <summary>
     /// Module parameter.
     /// </summary>
-    [Serializable()]
+    [Serializable]
     public class ModuleParameter
     {
+        private static readonly JsonSerializerSettings JsonSettings =
+            new JsonSerializerSettings
+            {
+                Culture = CultureInfo.InvariantCulture
+            };
+
         [NonSerialized]
         private ValueStatistics statistics;
+
         [NonSerialized]
         private DateTime requestUpdateTimestamp = DateTime.UtcNow;
+
         private object data;
-        //
+
         public ModuleParameter()
         {
-            // initialize
-            Name = "";
-            Value = "";
-            Description = "";
-            FieldType = "";
+            Name = string.Empty;
+            Value = string.Empty;
+            Description = string.Empty;
+            FieldType = string.Empty;
             UpdateTime = DateTime.UtcNow;
         }
 
         /// <summary>
         /// Gets the statistics.
         /// </summary>
-        /// <value>The statistics.</value>
         [XmlIgnore, JsonIgnore, BsonIgnore]
         public ValueStatistics Statistics
         {
             get
             {
-                if (statistics == null) statistics = new ValueStatistics();
-                return statistics;
+                return statistics ?? (statistics = new ValueStatistics());
             }
         }
 
         /// <summary>
         /// Gets the name.
         /// </summary>
-        /// <value>The name.</value>
         public string Name { get; set; }
 
         /// <summary>
         /// Gets the data object.
         /// </summary>
-        /// <returns></returns>
         public object GetData()
         {
             return data;
         }
 
         /// <summary>
-        /// If data is stored as a JSON serialized string, use this method to get the object instance specifying its type `T`.
+        /// If data is stored as a JSON serialized string, use this method to get the object instance specifying its type T.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <returns>The data object as type `T`.</returns>
-        /// <example>
-        /// Example:
-        /// <code>
-        /// // Sets the data as JSON serialized object
-        /// parameter.Value = "{ \"foo\": \"bar\", \"pippo\": \"pluto\" }";
-        /// var data = parameter.GetData<Dictionary<string,string>>();
-        /// foreach(var item in data) {
-        ///     // ...
-        /// }
-        /// </code>
-        /// </example>
         public T GetData<T>()
         {
             if (data is string)
@@ -107,7 +90,7 @@ namespace HomeGenie.Data
                 {
                     data = JsonConvert.DeserializeObject<T>(
                         Convert.ToString(data, CultureInfo.InvariantCulture),
-                        new JsonSerializerSettings() {Culture = CultureInfo.InvariantCulture}
+                        JsonSettings
                     );
                 }
                 catch
@@ -115,68 +98,77 @@ namespace HomeGenie.Data
                     // ignored
                 }
             }
+
             return (T)data;
         }
 
         /// <summary>
         /// Sets the data of this parameter.
         /// </summary>
-        /// <param name="dataObject"></param>
         public void SetData(object dataObject)
         {
             UpdateTime = DateTime.UtcNow;
             data = dataObject;
-            if (data!= null)
+
+            if (data == null)
+                return;
+
+            var stringValue = Value;
+
+            double numericValue;
+            if (!string.IsNullOrEmpty(stringValue) &&
+                double.TryParse(
+                    stringValue.Replace(",", "."),
+                    NumberStyles.Float | NumberStyles.AllowDecimalPoint,
+                    CultureInfo.InvariantCulture,
+                    out numericValue))
             {
-                // is this a numeric value that can be added for statistics?
-                string stringValue = Value;
-                double v;
-                if (!string.IsNullOrEmpty(stringValue) && double.TryParse(stringValue.Replace(",", "."), NumberStyles.Float | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out v))
-                {
-                    Statistics.AddValue(Name, v, UpdateTime);
-                }
+                Statistics.AddValue(Name, numericValue, UpdateTime);
             }
         }
 
         /// <summary>
-        /// Gets or sets the data of this parameter as string. If the value is a non-primitive object, set using the `setData` method, then the getter of `Value` will return the JSON serialized data.
+        /// Gets or sets the data as string.
         /// </summary>
-        /// <value>The string value.</value>
         public string Value
         {
             get
             {
-                bool isNumber = data is sbyte
-                                || data is byte
-                                || data is short
-                                || data is ushort
-                                || data is int
-                                || data is uint
-                                || data is long
-                                || data is ulong
-                                || data is float
-                                || data is double
-                                || data is decimal;
+                bool isNumber =
+                    data is sbyte ||
+                    data is byte ||
+                    data is short ||
+                    data is ushort ||
+                    data is int ||
+                    data is uint ||
+                    data is long ||
+                    data is ulong ||
+                    data is float ||
+                    data is double ||
+                    data is decimal;
+
                 if (isNumber || data is string)
                 {
                     return Convert.ToString(data, CultureInfo.InvariantCulture);
                 }
-                return JsonConvert.SerializeObject(data, new JsonSerializerSettings(){ Culture = CultureInfo.InvariantCulture });
+
+                return JsonConvert.SerializeObject(data, JsonSettings);
             }
-            set { SetData(value); }
+            set
+            {
+                SetData(value);
+            }
         }
 
         /// <summary>
         /// Gets or sets the description.
         /// </summary>
-        /// <value>The description.</value>
         [BsonIgnore]
         public string Description { get; set; }
 
         /// <summary>
         /// Gets or sets the type of the field.
         /// </summary>
-        /// <value>The type of the field.</value>
         [BsonIgnore]
         public string FieldType { get; set; }
 
@@ -186,7 +178,6 @@ namespace HomeGenie.Data
         /// <summary>
         /// Gets the update time.
         /// </summary>
-        /// <value>The update time.</value>
         public DateTime UpdateTime { get; set; }
 
         // TODO: deprecate this field
@@ -196,27 +187,35 @@ namespace HomeGenie.Data
         /// <summary>
         /// Gets the decimal value.
         /// </summary>
-        /// <value>The decimal value.</value>
         [XmlIgnore, JsonIgnore, BsonIgnore]
         public double DecimalValue
         {
             get
             {
-                string stringValue = Value;
-                double v = 0;
-                if (!String.IsNullOrEmpty(stringValue) && !double.TryParse(stringValue.Replace(",", "."), NumberStyles.Float | NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out v)) v = 0;
-                return v;
+                var stringValue = Value;
+
+                double value;
+                return !string.IsNullOrEmpty(stringValue) &&
+                       double.TryParse(
+                           stringValue.Replace(",", "."),
+                           NumberStyles.Float | NumberStyles.AllowDecimalPoint,
+                           CultureInfo.InvariantCulture,
+                           out value)
+                    ? value
+                    : 0d;
             }
         }
 
         /// <summary>
         /// Determines whether this instance has the given name.
         /// </summary>
-        /// <returns><c>true</c> if this instance is name; otherwise, <c>false</c>.</returns>
-        /// <param name="name">Name.</param>
         public bool Is(string name)
         {
-            return (this.Name.ToLower() == name.ToLower());
+            return string.Equals(
+                Name,
+                name,
+                StringComparison.OrdinalIgnoreCase
+            );
         }
 
         public void RequestUpdate()
@@ -227,20 +226,22 @@ namespace HomeGenie.Data
         /// <summary>
         /// Waits until this parameter is updated.
         /// </summary>
-        /// <returns><c>true</c>, if it was updated, <c>false</c> otherwise.</returns>
-        /// <param name="timeoutSeconds">Timeout seconds.</param>
         public bool WaitUpdate(double timeoutSeconds)
         {
-            var lastUpdate = UpdateTime;
-            while (lastUpdate.Ticks == UpdateTime.Ticks && (DateTime.UtcNow - requestUpdateTimestamp).TotalSeconds < timeoutSeconds)
+            var lastUpdateTicks = UpdateTime.Ticks;
+
+            while (lastUpdateTicks == UpdateTime.Ticks &&
+                   (DateTime.UtcNow - requestUpdateTimestamp).TotalSeconds < timeoutSeconds)
+            {
                 Thread.Sleep(250);
-            return lastUpdate.Ticks != UpdateTime.Ticks;
+            }
+
+            return lastUpdateTicks != UpdateTime.Ticks;
         }
 
         /// <summary>
-        /// Gets the idle time in seconds (time elapsed since last update).
+        /// Gets the idle time in seconds.
         /// </summary>
-        /// <value>The idle time (seconds).</value>
         [XmlIgnore, JsonIgnore, BsonIgnore]
         public double IdleTime
         {
@@ -249,7 +250,5 @@ namespace HomeGenie.Data
                 return (DateTime.UtcNow - UpdateTime).TotalSeconds;
             }
         }
-
     }
-
 }
