ffc7b4c496f0a13daa16c196b1ccf9f19c5b3e5c
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-06-19 16:42:26 +0500

    Update VirtualModule.cs

--- src/HomeGenie/Data/VirtualModule.cs
+++ src/HomeGenie/Data/VirtualModule.cs
@@ -1,38 +1,13 @@
-﻿/*
-   Copyright 2012-2025 G-Labs (https://github.com/genielabs)
-
-   This program is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Affero General Public License as
-   published by the Free Software Foundation, either version 3 of the
-   License, or (at your option) any later version.
-
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Affero General Public License for more details.
-
-   You should have received a copy of the GNU Affero General Public License
-   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
-/*
- *     Author: Generoso Martello <gene@homegenie.it>
- *     Project Homepage: https://homegenie.it
- */
-
 using System.Collections.Generic;
 
 namespace HomeGenie.Data
 {
     public class VirtualModule : Module
     {
-        public string ParentId { get; set; }
-        public bool IsActive = true;
-        public List<string> ImplementFeatures = new List<string>();
+        public string ParentId { get; set; } = string.Empty;
 
-        public VirtualModule()
-        {
-            ParentId = "";
-        }
+        public bool IsActive = true;
+
+        public readonly List<string> ImplementFeatures = new List<string>();
     }
 }
